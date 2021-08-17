87a580b6edd7456b6a849fe9323e87ace7c73d84
Alexander Leino <Alexander.Leino@yahoo.com>
2021-08-16 23:06:29 -0400

    added a function which allows me to upload a question when the next button is pushed

--- script.js
+++ script.js
@@ -1,10 +1,23 @@
+///When user opens when page the timer starts. Once the timer starts the user needs to click on one of the four options avaialable. Done
+/// Then the user will receive feedback if his answer was correct or not.  
+/// If the answer is correct then we will store the true value and update the counter
+/// Then the user will have to click next to continue.
+// Once the user clicks next then it will load another question and four answers. 
+// The user will keep doing this until time runs out
+// Then the user will be able to add his score to the high score list at the end and will be able to update his initials. 
+
+
+var questionEl = document.getElementById('question-body')
+var answerEl = document.getElementById('')
+var nextButton = document.getElementById('next-button')
+nextButton.addEventListener('click', nextQuestion)
 const startingMinutes = 1
 let time = startingMinutes * 60;
-
 const countdownEl = document.getElementById('time-keeper');
 
-setInterval(updateCountDown, 1000);
 
+/// This establishes the timer on the top right corner of the screen. 
+setInterval(updateCountDown, 1000);
 function updateCountDown() {
     const minutes = Math.floor(time/ 60);
     let seconds = time % 60;
@@ -19,3 +32,22 @@ function updateCountDown() {
         time = 0
     }
 }
+function nextQuestion (questions){
+ questionEl.innerText = questions.question
+}
+
+function selectAnswer(){
+
+}
+
+const questions = [
+    {
+        question: 'What does HTML an abbreviation for',
+        answers: [
+            { Text: 'Hypertext Markup Language', correct: true },
+            { text: 'How to make leeks', correct: false }
+        ]
+    }
+]
+console.log(question.value)
+console.log(question)
